80ca11e59e583e6656126599ffc78e1df07f3f6b
Manikanta T <manikanta.t@myntra.com>
2020-03-23 09:06:38 +0530

    Added a test class

---
+++ src/main/java/Test.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Manikanta Tummalapenta on 23 Mar 2020
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("This is mock repo!");
+    }
+}
